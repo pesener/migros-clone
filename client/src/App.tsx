66f615a8994a15435a86892bb56c7b8a2e823fa9
hakan pesener <pesener@windowslive.com>
2023-03-31 18:31:32 +0300

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MeyveSebze from "./components/Screens/MeyveSebze";
+import HomeScreen from "./components/Screens/HomeScreen";
+import SutKahval from "./components/Screens/SutKahval";
 
 function App() {
   const [loading, setLoading] = useState<boolean>(false);
@@ -11,7 +15,7 @@ function App() {
     }, 600);
   }, []);
   return (
-    <>
+    <Router>
       {loading ? (
         <div className="fixed inset-0 flex  justify-center items-center">
           <img
@@ -25,7 +29,17 @@ function App() {
           <Navbar />
         </div>
       )}
-    </>
+      <main className="py-10">
+        <div>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+
+            <Route path="/meyveSebze" element={<MeyveSebze />} />
+            <Route path="/sutKahval " element={<SutKahval />} />
+          </Routes>
+        </div>
+      </main>
+    </Router>
   );
 }
 
