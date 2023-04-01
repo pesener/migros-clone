9eb08121ba372f1a14d4330054834a448e7d5683
hakan pesener <pesener@windowslive.com>
2023-04-01 17:59:14 +0300

    Breakfast page div sizes, new div for filtering.

--- client/src/components/Screens/SutKahval.tsx
+++ client/src/components/Screens/SutKahval.tsx
@@ -3,8 +3,9 @@ import SutSlides from "../slides/sutSlides";
 
 const SutKahval = () => {
   return (
-    <div className="w-full h-[1500px] z-0 border">
-      <div className="mt-20 ml-[400px] z-0">
+    <div className="w-full h-[1500px] z-0 ">
+      <div className="h-[700px] mt-5 absolute w-[340px]  border-2 rounded ml-[50px]"></div>
+      <div className="ml-[420px] mt-12 absolute z-0">
         {" "}
         <SutSlides />
       </div>
--- client/src/components/slides/sutSlides.tsx
+++ client/src/components/slides/sutSlides.tsx
@@ -34,25 +34,25 @@ const SutSlides = () => {
     },
   ];
   return (
-    <div className="w-[1050px] h-[200px]  m-auto relative z-1 group">
+    <div className="w-[1070px] h-[205px]  m-auto relative z-1 group border-none outline-none">
       <div
         style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
         className="w-full h-full rounded bg-center bg-cover z-1"
       ></div>
       {/* left arrow */}
-      <div className="absolute z-2 flex items-center justify-center  top-[50%]  translate-x-[-30%] h-8 w-16 translate-y-[-50%] left-1 bg-white   rounded-tl-full rounded-tr-full  rotate-90  text-gray-600 cursor-pointer shadow-lg">
+      <div className="absolute z-2 flex items-center justify-center  top-[50%]  translate-x-[-31%] h-8 w-16 translate-y-[-50%] left-1 bg-white   rounded-tl-full rounded-tr-full  rotate-90  text-gray-600 cursor-pointer shadow-lg">
         <div className=" mt-2     ">
           <BsChevronDown onClick={prevSlide} size={30} />
         </div>
       </div>
       {/* rigth arrow */}
-      <div className="absolute flex items-center justify-center  top-[50%] translate-x-[55%] h-8 w-16 translate-y-[-50%] right-5 bg-white   rounded-tl-full rounded-tr-full  rotate-[270deg]  text-gray-600 cursor-pointer shadow-lg">
+      <div className="absolute flex items-center justify-center  top-[50%] translate-x-[56%] h-8 w-16 translate-y-[-50%] right-5 bg-white   rounded-tl-full rounded-tr-full  rotate-[270deg]  text-gray-600 cursor-pointer shadow-lg">
         <div className=" mt-2 z-2">
           {" "}
           <BsChevronDown onClick={nextSlide} size={30} />
         </div>
       </div>
-      <div className="flex top-1 justify-center py-2 cursor-pointer ">
+      <div className="flex top-0 justify-center py-2 cursor-pointer ">
         {slides.map((slide, slideIndex) => (
           <div
             key={slideIndex}
