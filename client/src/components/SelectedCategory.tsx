1431919d7cc8763ae257b26ea37f064528c769a7
hakan pesener <pesener@windowslive.com>
2023-04-02 22:04:46 +0300

    no need for map... fix

--- client/src/components/SelectedCategory.tsx
+++ client/src/components/SelectedCategory.tsx
@@ -19,33 +19,28 @@ const SelectedCategory = ({ id }: { id: any }) => {
 
   return (
     <div className="w-full h-[1500px] z-0 ">
-      {Object.keys(links)?.map((link: any) => (
-        <div className=" w-[250px] ml-[50px] flex items-center justify-center text-sm">
-          <div className="cursor-pointer mr-4">Anasayfa</div>{" "}
-          <BiChevronRight size={20} />{" "}
-          <div key={link._id} className=" cursor-default ml-4">
-            {link.name}
-          </div>{" "}
-        </div>
-      ))}
-      {Object.keys(links)?.map((link: any) => (
-        <div className="h-[700px] mt-5 absolute w-[340px]  border-2 rounded ml-[50px]">
-          <div className="h-[100px] border-b ">
-            {" "}
-            <h1 key={link._id} className="ml-6 font-bold text-xl mt-4">
-              {link.name}
+      <div className=" w-[250px] ml-[50px] flex items-center justify-center text-sm">
+        <div className="cursor-pointer mr-4">Anasayfa</div>{" "}
+        <BiChevronRight size={20} />{" "}
+        <div key={links._id} className=" cursor-default ml-4">
+          {links.name}
+        </div>{" "}
+      </div>
+
+      <div className="h-[700px] mt-5 absolute w-[340px]  border-2 rounded ml-[50px]">
+        <div className="h-[100px] border-b ">
+          {" "}
+          <h1 key={links._id} className="ml-6 font-bold text-xl mt-4">
+            {links.name}
+          </h1>
+          {links.sublinks?.map((mysublinks: any) => (
+            <h1 key={mysublinks._id} className="ml-6 font-normal text-md mt-4">
+              {mysublinks.length}
             </h1>
-            {link.sublinks?.map((mysublinks: any) => (
-              <h1
-                key={mysublinks._id}
-                className="ml-6 font-normal text-md mt-4"
-              >
-                {mysublinks.length}
-              </h1>
-            ))}
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
+
       <div className="ml-[420px] mt-12 absolute z-0">
         {" "}
         <SutSlides />
