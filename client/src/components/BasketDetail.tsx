01f9aaa2fa58e333fe639b15c1e74a1d7069a9b3
hakan pesener <pesener@windowslive.com>
2023-05-08 18:45:44 +0300

    Finish // basket detail page design

--- client/src/components/BasketDetail.tsx
+++ client/src/components/BasketDetail.tsx
@@ -73,109 +73,169 @@ const BasketDetail = ({
           </div>
         </div>
       </div>
-      <div className="h-[700px]">
-        {" "}
-        <div className="w-[1000px]   flex ">
+      <div className="flex justify-between">
+        <div className="h-[700px]">
           {" "}
-          <div className="flex w-[1000px] justify-between">
-            <div className="flex ">
-              {" "}
-              <div className=" ml-[125px] h-[40px] text-[27px] font-bold mt-[40px]">
-                Sepetim
+          <div className="w-[1000px]   flex ">
+            {" "}
+            <div className="flex w-[1000px] justify-between">
+              <div className="flex ">
+                {" "}
+                <div className=" ml-[125px] h-[40px] text-[27px] font-bold mt-[40px]">
+                  Sepetim
+                </div>
+                <div className="text-md  h-[40px] text-gray-400 ml-2 font-semibold mt-[52px]">
+                  {cardItems.length} Ürün
+                </div>
               </div>
-              <div className="text-md  h-[40px] text-gray-400 ml-2 font-semibold mt-[52px]">
+              <div className="text-primary font-bold  h-[40px] text-[16px] mt-[54px] mr-4 cursor-pointer">
+                Sepeti Düzenle
+              </div>
+            </div>
+          </div>
+          <div>
+            {cardItems?.map((item: any) => (
+              <div className="border border-gray-300 ml-[120px] rounded-lg w-[870px] h-[165px] mb-4 flex">
+                {" "}
+                <div>
+                  {" "}
+                  <img
+                    className="w-[140px] h-[140px] mt-3"
+                    src={item.quantImg}
+                    alt=""
+                  />
+                </div>
+                <div className="font-semibold text-md mt-3 flex flex-col w-[400px]">
+                  {" "}
+                  {item.quantName}
+                  <div className="bg-[#fdf2b8] text-[11px] font-semibold rounded w-[80px] mt-1 justify-center h-[20px] items-center flex">
+                    {" "}
+                    İndirimli ürün{" "}
+                  </div>
+                </div>
+                <div className="flex flex-col ml-[200px]">
+                  <div className="bg-[#fce24d] font-bold  w-[120px] rounded-tl-full rounded-r-full items-center  justify-center h-[48px] text-black text-[19px]   mt-4  flex   ">
+                    {item.quantity > 1
+                      ? (item.quantPrice * item.quantity).toFixed(2)
+                      : item.quantPrice}
+                    <div className="text-[12px] font-semibold mt-1 ">
+                      {"\u00A0 TL"}
+                    </div>
+                  </div>
+                  <div className="flex border-gray-100 border-2 rounded-lg w-[100px] h-[34px] justify-between  mt-12">
+                    {" "}
+                    <div
+                      className={`${
+                        item.quantity > 2 || item.quantity === 2
+                          ? "text-gray-500 bg-[#f5f7fa] rounded-tl rounded-bl  w-[32px] h-[32px] flex justify-center items-center "
+                          : "hidden"
+                      } `}
+                    >
+                      <AiOutlineMinus
+                        className={`${
+                          item.quantity > 2 || item.quantity === 2
+                            ? "text-gray-500 bg-[#f5f7fa] rounded-tl rounded-bl  w-[20px] h-[20px] cursor-pointer "
+                            : "hidden"
+                        } `}
+                        onClick={() => {
+                          handleProductCountMinus(item.quantId);
+                        }}
+                      />
+                    </div>
+                    <div
+                      className={`${
+                        item.quantity < 2 || item.quantity === 1
+                          ? "w-[32px] bg-[#f5f7fa] h-[32px] flex items-center justify-center cursor-pointer"
+                          : "hidden"
+                      } `}
+                    >
+                      {" "}
+                      <BsTrash3
+                        className={`${
+                          item.quantity < 2 || item.quantity === 1
+                            ? "text-gray-500 bg-[#f5f7fa] rounded-tl rounded-bl w-[20px] h-[20px] "
+                            : "hidden"
+                        } `}
+                        onClick={() => {
+                          removeFromCard(item.quantId);
+                        }}
+                      />{" "}
+                    </div>
+                    <div className="flex flex-col text-xs font-semibold justify-center items-center">
+                      {item.quantity}{" "}
+                      <div className="text-xs font-normal"> adet</div>
+                    </div>
+                    <div className="text-gray-500 bg-[#f5f7fa] justify-center items-center flex   w-[32px] h-[32px] rounded-tr rounded-br  cursor-pointer">
+                      <AiOutlinePlus
+                        className="text-gray-500 bg-[#f5f7fa]   w-[20px] h-[20px] rounded-tr rounded-br "
+                        onClick={() => {
+                          handleProductCount(item.quantId);
+                        }}
+                      />{" "}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* sağ Taraf */}
+
+        <div className="w-[550px]  h-[320px] flex  items-center">
+          <div className="border border-gray-300 rounded-lg w-[420px] h-[320px] mt-[70px] ml-8 flex flex-col">
+            <div className="flex mt-4 ml-6 justify-between h-[40px] ">
+              <div className="text-lg  font-bold ">Sepet Özeti</div>{" "}
+              <div className="mr-4 text-md text-gray-500">
                 {cardItems.length} Ürün
               </div>
             </div>
-            <div className="text-primary font-bold  h-[40px] text-[16px] mt-[54px] mr-4 cursor-pointer">
-              Sepeti Düzenle
+            <div className="flex mt-4 ml-6 justify-between h-[40px] ">
+              <div className="text-md text-gray-500 font-semibold ">
+                Toplam Tutar
+              </div>{" "}
+              <div className="mr-4 text-md font-semibold text-gray-900">
+                {cardItems?.map((item: any) =>
+                  cardItems.reduce((total: any, cartItem: any) => {
+                    cardItems?.find((i: any) => i.quantId === cartItem.quantId);
+                    const itemsCount: Number =
+                      total + (item?.quantPrice || 0) * cartItem.quantity;
+                    console.log(total, "www", itemsCount);
+
+                    return itemsCount;
+                  }, 0)
+                )}{" "}
+                TL
+              </div>
+            </div>
+            <div className="flex mt-4 ml-6 justify-between h-[40px] ">
+              <div className="text-md text-[#02b61d] font-semibold ">
+                Migros İndirimi
+              </div>{" "}
+              <div className="mr-4 text-md text-[#02b61d] ">-0,00 TL</div>
+            </div>
+            <div className="flex mt-4 ml-6 justify-between h-[40px] ">
+              <div className="text-lg  font-bold ">Ödenecek Tutar</div>{" "}
+              <div className="mr-4 text-md font-semibold  ">
+                {" "}
+                {cardItems?.map((item: any) =>
+                  cardItems.reduce((total: any, cartItem: any) => {
+                    cardItems?.find((i: any) => i.quantId === cartItem.quantId);
+                    const itemsCount: Number =
+                      total + (item?.quantPrice || 0) * cartItem.quantity;
+                    console.log(total, "www", itemsCount);
+
+                    return itemsCount;
+                  }, 0)
+                )}{" "}
+                TL
+              </div>
+            </div>
+            <div className="flex border-t justify-center items-center h-[100px] ">
+              <div className="border w-[400px] h-[60px] flex justify-center bg-primary text-white font-bold text-md rounded-lg items-center cursor-pointer">
+                Devam Et
+              </div>{" "}
             </div>
           </div>
-        </div>
-        <div>
-          {cardItems?.map((item: any) => (
-            <div className="border border-gray-300 ml-[120px] rounded-lg w-[870px] h-[165px] mb-4 flex">
-              {" "}
-              <div>
-                {" "}
-                <img
-                  className="w-[140px] h-[140px] mt-3"
-                  src={item.quantImg}
-                  alt=""
-                />
-              </div>
-              <div className="font-semibold text-md mt-3 flex flex-col w-[400px]">
-                {" "}
-                {item.quantName}
-                <div className="bg-[#fdf2b8] text-[11px] font-semibold rounded w-[80px] mt-1 justify-center h-[20px] items-center flex">
-                  {" "}
-                  İndirimli ürün{" "}
-                </div>
-              </div>
-              <div className="flex flex-col ml-[200px]">
-                <div className="bg-[#fce24d] font-bold  w-[120px] rounded-tl-full rounded-r-full items-center  justify-center h-[48px] text-black text-[19px]   mt-4  flex   ">
-                  {item.quantity > 1
-                    ? (item.quantPrice * item.quantity).toFixed(2)
-                    : item.quantPrice}
-                  <div className="text-[12px] font-semibold mt-1 ">
-                    {"\u00A0 TL"}
-                  </div>
-                </div>
-                <div className="flex border-gray-100 border-2 rounded-lg w-[100px] h-[34px] justify-between  mt-12">
-                  {" "}
-                  <div
-                    className={`${
-                      item.quantity > 2 || item.quantity === 2
-                        ? "text-gray-500 bg-[#f5f7fa] rounded-tl rounded-bl  w-[32px] h-[32px] flex justify-center items-center "
-                        : "hidden"
-                    } `}
-                  >
-                    <AiOutlineMinus
-                      className={`${
-                        item.quantity > 2 || item.quantity === 2
-                          ? "text-gray-500 bg-[#f5f7fa] rounded-tl rounded-bl  w-[20px] h-[20px] cursor-pointer "
-                          : "hidden"
-                      } `}
-                      onClick={() => {
-                        handleProductCountMinus(item.quantId);
-                      }}
-                    />
-                  </div>
-                  <div
-                    className={`${
-                      item.quantity < 2 || item.quantity === 1
-                        ? "w-[32px] bg-[#f5f7fa] h-[32px] flex items-center justify-center cursor-pointer"
-                        : "hidden"
-                    } `}
-                  >
-                    {" "}
-                    <BsTrash3
-                      className={`${
-                        item.quantity < 2 || item.quantity === 1
-                          ? "text-gray-500 bg-[#f5f7fa] rounded-tl rounded-bl w-[20px] h-[20px] "
-                          : "hidden"
-                      } `}
-                      onClick={() => {
-                        removeFromCard(item.quantId);
-                      }}
-                    />{" "}
-                  </div>
-                  <div className="flex flex-col text-xs font-semibold justify-center items-center">
-                    {item.quantity}{" "}
-                    <div className="text-xs font-normal"> adet</div>
-                  </div>
-                  <div className="text-gray-500 bg-[#f5f7fa] justify-center items-center flex   w-[32px] h-[32px] rounded-tr rounded-br  cursor-pointer">
-                    <AiOutlinePlus
-                      className="text-gray-500 bg-[#f5f7fa]   w-[20px] h-[20px] rounded-tr rounded-br "
-                      onClick={() => {
-                        handleProductCount(item.quantId);
-                      }}
-                    />{" "}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </div>
